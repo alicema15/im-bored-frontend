2011f593782ac2254140a0da04a33d10cf61b2c3
alicema15 <alicema@berkeley.edu>
2020-03-21 20:17:08 -0400

    FEATURE: add react-router, add a route for / path and /calendar path

---
+++ src/CalendarPage/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+
+class CalendarPage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>Calendar</div>
+        </header>
+      </div>
+    )
+  }
+
+}
+
+export default CalendarPage;
