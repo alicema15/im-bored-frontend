54e53b794e5e45b1fbed578d44d220e322240b8a
alicema15 <alicema@berkeley.edu>
2020-03-22 12:25:08 -0400

    AESTHETICS: add device container

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb, Tag, Button } from 'antd';
+import Device from "react-device-frame";
+
 import axios from 'axios';
 
 import './index.css';
@@ -33,14 +35,16 @@ class HomePage extends Component {
         <Header className="header">Header</Header>
         <Content className="content h-100">
           <div className="row h-100">
-            <div className="col col-md-6">
-              <div>Photo</div>
+            <div className="col col-md-6 device-container">
+              <Device className="device" name="iphone-x" url="https://pbs.twimg.com/media/C5xHw4lXEAEAnNK.jpg">
+                <div>Photo</div>
+              </Device>
             </div>
             <div className="col col-md-6 vertical-center h-100">
               <div className="welcome-text">
                 <Tag color="green">Online</Tag>
                 <div className="header-text">Alice is available to chat today</div>
-                <div className="sub-text">Sync your calendar and we'll find the best time to chat.</div>
+                <p className="sub-text">Sync your calendar and we'll find the best time to chat.</p>
                 <Button type="primary" onClick={ this.routeChange }>Talk to Alice</Button>
               </div>
             </div>
